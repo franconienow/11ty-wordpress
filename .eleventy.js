7b2ace978d054d7c476302af20b45090f5902425
franconienow <fnienow.sn@gmail.com>
2022-05-02 09:24:21 -0300

    Posts relacionados

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,38 @@ module.exports = (eleventyConfig) => {
     }
   });
 
+  eleventyConfig.addLiquidFilter("getRelatedPosts", (posts, catList, curId) => {
+    const relatedPosts = [];
+    for (const curCategory of catList) {
+      for (const post of posts) {
+        if (post.categories.includes(curCategory) && post.id != curId) {
+          if (!relatedPosts.map((rPost) => rPost.id).includes(post.id)) {
+            relatedPosts.push(post);
+          }
+        }
+      }
+    }
+
+    const shuffleArray = (array) => {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+      return array
+    };
+
+    if (relatedPosts.length > 0) {
+      const shuffled = shuffleArray(Array.from(relatedPosts));
+      return shuffled.slice(0, 2);
+    }
+
+    return false;
+
+    // TODO: Testar shuffle
+  });
+
   eleventyConfig.ignores.add(path.join("src", "assets", "/**"));
 
   eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
