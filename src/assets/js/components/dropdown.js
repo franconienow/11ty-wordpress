b642c2e84945b0be02b3f2f32aba54abbf00b13e
franconienow <fnienow.sn@gmail.com>
2022-05-01 23:07:36 -0300

    Mais componentes e página single post

--- src/assets/js/components/dropdown.js
+++ src/assets/js/components/dropdown.js
@@ -1,9 +1,9 @@
 const initDropdowns = (elTarget = false) => {
   let allDropdowns;
   if (elTarget) {
-    allDropdowns = elTarget.querySelectorAll(".dropdown-toggler");
+    allDropdowns = elTarget.querySelectorAll(".dropdown__toggler");
   } else {
-    allDropdowns = document.body.querySelectorAll(".dropdown-toggler");
+    allDropdowns = document.body.querySelectorAll(".dropdown__toggler");
   }
   allDropdowns.forEach((dropdownEl) => {
     makeDropdown(dropdownEl);
@@ -14,9 +14,20 @@ const makeDropdown = (dropdownEl) => {
   dropdownEl.addEventListener("click", (event) => {
     event.preventDefault();
     const target = dropdownEl.nextSibling;
-    if (!target.classList.contains("dropdown-target")) return;
+    if (!target.classList.contains("dropdown__target")) return;
     target.classList.toggle("active");
+    toggleAriaAtt(dropdownEl);
   });
+};
+
+const toggleAriaAtt = (el) => {
+  const value = el.getAttribute("aria-expanded");
+  console.log(value);
+  if (value == "false") {
+    el.setAttribute("aria-expanded", "true");
+  } else {
+    el.setAttribute("aria-expanded", "false");
+  }
 };
 
 export { initDropdowns, makeDropdown };
---
+++ src/data/home.js
@@ -0,0 +1,10 @@
+const EleventyFetch = require("@11ty/eleventy-fetch");
+
+module.exports = async () => {
+  const url = "https://admin.bigtuna.site/wp-json/wp/v2/pages/9?acf_format=standard";
+  return EleventyFetch(url, {
+    directory: ".11ty.cache",
+    duration: "30m",
+    type: "json",
+  });
+};
